aa3f403043c86c3f38783af1f59d1db8aa74eb8b
Linesh <linesh.simpcity@gmail.com>
2020-02-02 17:01:08 +0800

    Revert "fix:change String targetUnit to Enum Unit.problem:constructor"
    This reverts commit 17ff5f43574cc73684fedfbe703ce141c5caff0d.

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,44 +1,38 @@
 public class Length {
     private final double value;
     private final String unit;
-    private Unit temp_unit;
     public static final String FOOT = "foot";
     public static final String YARD = "yard";
     public static final String INCH = "inch";
 
     public Length(double value, String unit) {
-        this(value, unit, null);
-    }
-
-    public Length(double value, String unit, Unit temp_unit) {
         this.value = value;
         this.unit = unit;
-        this.temp_unit = temp_unit;
     }
 
-    public Length as(String targetUnit, Unit temp_unit) {
+    public Length as(String targetUnit) {
         Length result = this;
 
-        if (this.temp_unit == Unit.FOOT) {
-            if (temp_unit == Unit.YARD) {
+        if (this.unit.equals(Length.FOOT)) {
+            if (targetUnit.equals(Length.YARD)) {
                 result = new Length(this.value / 3, targetUnit);
-            } else if (temp_unit == Unit.INCH) {
+            } else if (targetUnit.equals(Length.INCH)) {
                 result = new Length(this.value * 12, targetUnit);
             }
         }
 
-        if (this.temp_unit == Unit.YARD) {
-            if (temp_unit == Unit.INCH) {
+        if (this.unit.equals(Length.YARD)) {
+            if (targetUnit.equals(Length.INCH)) {
                 result = new Length(this.value * 36, targetUnit);
-            } else if (temp_unit == Unit.FOOT) {
+            } else if (targetUnit.equals(Length.FOOT)){
                 result = new Length(this.value * 3, targetUnit);
             }
         }
 
-        if (this.temp_unit == Unit.INCH) {
-            if (temp_unit == Unit.FOOT) {
+        if (this.unit.equals(Length.INCH)) {
+            if (targetUnit.equals(Length.FOOT)) {
                 result = new Length(this.value / 12, targetUnit);
-            } else if (temp_unit == Unit.YARD) {
+            } else if (targetUnit.equals(Length.YARD)) {
                 result = new Length(this.value / 36, targetUnit);
             }
         }
--- src/main/java/Unit.java
+++
@@ -1,3 +0,0 @@
-public enum Unit {
-    INCH, FOOT, YARD
-}
