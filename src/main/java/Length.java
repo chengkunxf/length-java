95e9336044c588e543cf5f3150e04a8547e3879e
Linesh <linesh.simpcity@gmail.com>
2020-02-02 17:06:06 +0800

    doing the same where targetUnit is INCH

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -15,6 +15,9 @@ public class Length {
         if (targetUnit.equals(Length.FOOT)) {
             temp_unit = Unit.FOOT;
         }
+        if (targetUnit.equals(Length.INCH)) {
+            temp_unit = Unit.INCH;
+        }
         return temp_as(targetUnit, temp_unit);
     }
 
@@ -24,13 +27,13 @@ public class Length {
         if (this.unit.equals(Length.FOOT)) {
             if (targetUnit.equals(Length.YARD)) {
                 result = new Length(this.value / 3, targetUnit);
-            } else if (targetUnit.equals(Length.INCH)) {
+            } else if (temp_unit == Unit.INCH) {
                 result = new Length(this.value * 12, targetUnit);
             }
         }
 
         if (this.unit.equals(Length.YARD)) {
-            if (targetUnit.equals(Length.INCH)) {
+            if (temp_unit == Unit.INCH) {
                 result = new Length(this.value * 36, targetUnit);
             } else if (temp_unit == Unit.FOOT){
                 result = new Length(this.value * 3, targetUnit);
