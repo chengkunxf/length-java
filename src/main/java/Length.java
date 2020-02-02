ff5b089657c93ec7a41f6c385465e445c13dde68
Linesh <linesh.simpcity@gmail.com>
2020-02-02 17:17:11 +0800

    now getUnit() is unused

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -69,10 +69,6 @@ public class Length {
         return this.value;
     }
 
-    public String getUnit() {
-        return this.unit;
-    }
-
     public Unit temp_getUnit() {
         return this.temp_unit;
     }
