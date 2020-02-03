c851e4b39f091b059e04e4cff345c03e9db9b970
chengkunxf <chengkunxf@126.com>
2020-02-04 00:12:39 +0800

    refactor:as函数中的构造函数也需要重构，增加新的构造函数，旧的调用新的，将之前 String转 Enum 的逻辑拷贝到旧的构造函数中

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -4,43 +4,60 @@ public class Length {
     public static final String INCH = "inch";
     private final double value;
     private final String unit;
+    private Unit temp_unit;
 
     public Length(double value, String unit) {
+        this(value, unit, convertStringToEnum(unit));
+    }
+
+    private static Unit convertStringToEnum(String unit) {
+        Unit temp_unit = null;
+        if (unit.equals(Length.YARD)) {
+            temp_unit = Unit.YARD;
+        }
+        if (unit.equals(Length.INCH)) {
+            temp_unit = Unit.INCH;
+        }
+        if (unit.equals(Length.FOOT)) {
+            temp_unit = Unit.FOOT;
+        }
+        return temp_unit;
+    }
+
+    public Length(double value, String unit, Unit temp_unit) {
         this.value = value;
         this.unit = unit;
+        this.temp_unit = temp_unit;
     }
 
 
     public Length as(String targetUnit) {
-        Unit temp_unit = null;
-        if(targetUnit.equals(Length.YARD)){
-            temp_unit = Unit.YARD;
-        }
+        Unit temp_unit = convertStringToEnum(targetUnit);
         return temp_as(targetUnit, temp_unit);
     }
 
     public Length temp_as(String targetUnit, Unit temp_unit) {
         Length result = this;
-        if (this.unit.equals(FOOT)) {
+        if (this.temp_unit == Unit.FOOT) {
             if (temp_unit == Unit.YARD) {
                 result = new Length(this.value / 3, targetUnit);
-            } else if (targetUnit.equals(INCH)) {
+            } else if (temp_unit == Unit.INCH) {
                 result = new Length(this.value * 12, targetUnit);
             }
         }
 
         if (this.unit.equals(YARD)) {
-            if (targetUnit.equals(INCH)) {
+            if (temp_unit == Unit.INCH) {
                 result = new Length(this.value * 36, targetUnit);
-            } else if (targetUnit.equals(FOOT)) {
+            } else if (temp_unit == Unit.FOOT) {
                 result = new Length(this.value * 3, targetUnit);
             }
         }
 
         if (this.unit.equals(INCH)) {
-            if (targetUnit.equals(FOOT)) {
+            if (temp_unit == Unit.FOOT) {
                 result = new Length(this.value / 12, targetUnit);
-            } else if (targetUnit.equals(YARD)) {
+            } else if (temp_unit == Unit.YARD) {
                 result = new Length(this.value / 36, targetUnit);
             }
         }
