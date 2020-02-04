1a0e1c456c8324d5f85cedec224d26b927d435e9
chengkunxf <chengkunxf@126.com>
2020-02-04 11:11:11 +0800

    refactor:替换构造函数 as 的引用为 temp_as,发现错误，先去掉 temp_as 的多余参数 targetUnit

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -29,33 +29,32 @@ public class Length {
     }
 
     public Length as(String targetUnit) {
-        Unit temp_unit = convertStringToEnum(targetUnit);
-        return temp_as(targetUnit, temp_unit);
+        return temp_as(convertStringToEnum(targetUnit));
     }
 
-    public Length temp_as(String targetUnit, Unit temp_unit) {
+    public Length temp_as(Unit temp_unit) {
         Length result = this;
         if (this.temp_unit == Unit.FOOT) {
             if (temp_unit == Unit.YARD) {
-                result = new Length(this.value / 3, targetUnit);
+                result = new Length(this.value / 3, temp_unit);
             } else if (temp_unit == Unit.INCH) {
-                result = new Length(this.value * 12, targetUnit);
+                result = new Length(this.value * 12, temp_unit);
             }
         }
 
         if (this.temp_unit == Unit.YARD) {
             if (temp_unit == Unit.INCH) {
-                result = new Length(this.value * 36, targetUnit);
+                result = new Length(this.value * 36, temp_unit);
             } else if (temp_unit == Unit.FOOT) {
-                result = new Length(this.value * 3, targetUnit);
+                result = new Length(this.value * 3, temp_unit);
             }
         }
 
         if (this.temp_unit == Unit.INCH) {
-            if (targetUnit.equals(FOOT)) {
-                result = new Length(this.value / 12, targetUnit);
+            if (temp_unit == Unit.FOOT) {
+                result = new Length(this.value / 12, temp_unit);
             } else if (temp_unit == Unit.YARD) {
-                result = new Length(this.value / 36, targetUnit);
+                result = new Length(this.value / 36, temp_unit);
             }
         }
 
