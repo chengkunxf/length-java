df8b1d87ca32c0b9f58e2e53704c43f0134e77d0
chengkunxf <chengkunxf@126.com>
2020-02-06 08:29:09 +0800

    refactor:as 函数去掉 targetUnit 的引用，需要重构构造函数，增加新的构造函数，增加旧字段到新字段的转换

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -7,7 +7,21 @@ public class Length {
     private Unit temp_unit;
 
     public Length(double value, String unit) {
-        this(value, unit, null);
+        this(value  , unit, convertStringToEnum(unit));
+    }
+
+    private static Unit convertStringToEnum(String unit) {
+        Unit temp_unit = null;
+        if (unit.equals(YARD)) {
+            temp_unit = Unit.YARD;
+        }
+        if (unit.equals(INCH)) {
+            temp_unit = Unit.INCH;
+        }
+        if (unit.equals(FOOT)) {
+            temp_unit = Unit.FOOT;
+        }
+        return temp_unit;
     }
 
     public Length(double value, String unit, Unit temp_unit) {
@@ -17,16 +31,7 @@ public class Length {
     }
 
     public Length as(String targetUnit) {
-        Unit temp_unit = null;
-        if (targetUnit.equals(YARD)) {
-            temp_unit = Unit.YARD;
-        }
-        if (targetUnit.equals(INCH)) {
-            temp_unit = Unit.INCH;
-        }
-        if (targetUnit.equals(FOOT)) {
-            temp_unit = Unit.FOOT;
-        }
+        Unit temp_unit = convertStringToEnum(targetUnit);
         return temp_as(targetUnit, temp_unit);
     }
 
