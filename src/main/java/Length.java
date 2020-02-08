d8d7e29cb35b6475930905a3b8df60b35e7f5acd
chengkunxf <chengkunxf@126.com>
2020-02-08 08:37:09 +0800

    refactor:extract function,抽取变化,将变量声明移动到使用处之前

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -12,27 +12,28 @@ public class Length {
 
     public Length as(Unit unit) {
         Length result = this;
+        double value = this.value;
         if (this.unit == Unit.FOOT) {
             if (unit == Unit.YARD) {
-                result = new Length(this.value / 3, unit);
+                result = new Length(value / 3, unit);
             } else if (unit == Unit.INCH) {
-                result = new Length(this.value * 12, unit);
+                result = new Length(value * 12, unit);
             }
         }
 
         if (this.unit == Unit.YARD) {
             if (unit == Unit.INCH) {
-                result = new Length(this.value * 36, unit);
+                result = new Length(value * 36, unit);
             } else if (unit == Unit.FOOT) {
-                result = new Length(this.value * 3, unit);
+                result = new Length(value * 3, unit);
             }
         }
 
         if (this.unit == Unit.INCH) {
             if (unit == Unit.FOOT) {
-                result = new Length(this.value / 12, unit);
+                result = new Length(value / 12, unit);
             } else if (unit == Unit.YARD) {
-                result = new Length(this.value / 36, unit);
+                result = new Length(value / 36, unit);
             }
         }
 
