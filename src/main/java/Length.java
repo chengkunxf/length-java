f724c4a229946a3d0c7d0fe6a217c6667b85c46c
chengkunxf <chengkunxf@126.com>
2020-02-08 16:55:02 +0800

    refactor:extract calculateVale function

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -11,6 +11,10 @@ public class Length {
     }
 
     public Length as(Unit temp_unit) {
+        return new Length(calculateValue(temp_unit), temp_unit);
+    }
+
+    private double calculateValue(Unit temp_unit) {
         double value = this.value;
         if (this.unit == Unit.FOOT) {
             if (temp_unit == Unit.YARD) {
@@ -35,8 +39,7 @@ public class Length {
                 value = value / 36;
             }
         }
-
-        return new Length(value, temp_unit);
+        return value;
     }
 
     public double getValue() {
