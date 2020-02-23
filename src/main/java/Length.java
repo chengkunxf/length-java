ffe84584ba02dff808cdf2b81d0fa6db821d59f8
chengkunxf <chengkunxf@126.com>
2020-02-23 21:51:02 +0800

    refactor:rename targetUnit

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,35 +1,38 @@
 public class Length {
+    public static final String FOOT = "f";
+    public static final String YARD = "yard";
+    public static final String INCH = "inch";
     private final double value;
     private final String unit;
 
-    public Length(double val, String uinnt) {
+    public Length(double val, String unit) {
         this.value = val;
-        this.unit = uinnt;
+        this.unit = unit;
     }
 
-    public Length as(String u) {
+    public Length as(String tagetUnit) {
         Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
-                len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
-                len = new Length(this.value * 12, u);
+        if (this.unit.equals(FOOT)) {
+            if (tagetUnit.equals(YARD)) {
+                len = new Length(this.value / 3, tagetUnit);
+            } else if (tagetUnit.equals(INCH)) {
+                len = new Length(this.value * 12, tagetUnit);
             }
         }
 
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
-                len = new Length(this.value * 36, u);
-            } else if (u.equals("f")){
-                len = new Length(this.value * 3, u);
+        if (this.unit.equals(YARD)) {
+            if (tagetUnit.equals(INCH)) {
+                len = new Length(this.value * 36, tagetUnit);
+            } else if (tagetUnit.equals(FOOT)){
+                len = new Length(this.value * 3, tagetUnit);
             }
         }
 
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
-                len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
-                len = new Length(this.value / 36, u);
+        if (this.unit.equals(INCH)) {
+            if (tagetUnit.equals(FOOT)) {
+                len = new Length(this.value / 12, tagetUnit);
+            } else if (tagetUnit.equals(YARD)) {
+                len = new Length(this.value / 36, tagetUnit);
             }
         }
 
